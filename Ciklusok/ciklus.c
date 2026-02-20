e6ba06a103a6164d05d3f3bc333dabec0d28a074
Kristofgitgud <burszemkristof@gmail.com>
2026-02-20 16:15:21 +0100

    Add files via upload

---
+++ Ciklusok/ciklus.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+
+
+        printf("Hello!\n"); 
+
+
+        for(int i=0;i<5;i++){
+            printf("ciklus számláló: %d\n",i);
+        }
+    return 0;
+}
---
+++ Ciklusok/tizszaz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+
+    printf("for-ral\n");
+    for(int i=10;i<101;i++){
+        printf("%d\n",i);
+    };
+
+        printf("while-al\n");
+        int i=10;
+        while(i<101){
+            printf("%d\n",i);
+            i++;
+        };
+    return 0;
+}
