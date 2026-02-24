2c06e57d9fa6b367f82ffd260a66feff22c11635
Kristofgitgud <burszemkristof@gmail.com>
2026-02-24 16:19:25 +0100

    Add files via upload

---
+++ Ciklusok/egytolszazig.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+ int osszeg=0;
+        for(int i=1;i<=100;i++){
+            osszeg=osszeg+i;
+        }
+            printf("A számok összege 1-től százig: %d\n",osszeg);
+
+return 0;
+}
